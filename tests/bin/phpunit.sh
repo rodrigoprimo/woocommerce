10752186d598621b75e8325b5fdde9b805b3ef9d
Rodrigo Primo <rodrigosprimo@gmail.com>
2018-12-05 11:52:07 -0200

    Use PHPUnit installed manually via composer

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -2,6 +2,6 @@
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 	phpdbg -qrr -d memory_limit=-1 $HOME/.composer/vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
 else
-	phpunit --version
-	phpunit -c phpunit.xml
+	$HOME/.config/composer/vendor/bin/phpunit --version
+	composer exec -v --  "phpunit -c phpunit.xml"
 fi
