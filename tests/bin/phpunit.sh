086e6add6af939fdb43060b55340db5705d7d081
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-01-21 15:36:07 -0200

    Changes to make it easier to debug the problem

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -7,5 +7,5 @@ fi
 if [[ ${RUN_CODE_COVERAGE} == 1 ]]; then
 	phpdbg -qrr -d memory_limit=-1 $HOME/.composer/vendor/bin/phpunit -c phpunit.xml --coverage-clover=coverage.clover --exclude-group=timeout
 else
-	phpunit -c phpunit.xml
+	phpunit -c phpunit.xml tests/unit-tests/api/functions.php
 fi
