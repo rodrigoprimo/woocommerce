3260f52ae6d6c50897a5f9b18ff7966ffbdb44a7
Rodrigo Primo <rodrigosprimo@gmail.com>
2019-01-21 15:28:21 -0200

    Test to try to understand why those tests are failing on php 5.2 and 5.3

--- tests/unit-tests/api/functions.php
+++ tests/unit-tests/api/functions.php
@@ -27,35 +27,6 @@ class WC_Tests_API_Functions extends WC_Unit_Test_Case {
 	private $file_name;
 
 	/**
-	 * Run setup code for unit tests.
-	 */
-	public function setUp() {
-		parent::setUp();
-
-		// Callback used by WP_HTTP_TestCase to decide whether to perform HTTP requests or to provide a mocked response.
-		$this->http_responder = array( $this, 'mock_http_responses' );
-
-		$upload_dir_info       = wp_upload_dir();
-		$this->upload_dir_path = $upload_dir_info['path'];
-		$this->upload_dir_url  = $upload_dir_info['url'];
-		$this->file_name       = 'Dr1Bczxq4q.png';
-	}
-
-	/**
-	 * Run tear down code for unit tests.
-	 */
-	public function tearDown() {
-		parent::tearDown();
-
-		// remove files created in the wc_rest_upload_image_from_url() tests.
-		$file_path = $this->upload_dir_path . '/' . $this->file_name;
-
-		if ( file_exists( $file_path ) ) {
-			unlink( $file_path );
-		}
-	}
-
-	/**
 	 * Test wc_rest_prepare_date_response().
 	 *
 	 * @since 2.6.0
@@ -68,8 +39,8 @@ class WC_Tests_API_Functions extends WC_Unit_Test_Case {
 	 * Test wc_rest_upload_image_from_url() should return error when unable to download image.
 	 */
 	public function test_wc_rest_upload_image_from_url_should_return_error_when_unable_to_download_image() {
-		$expected_error_message = 'Error getting remote image http://somedomain.com/nonexistent-image.png. Error: Not found.';
-		$result                 = wc_rest_upload_image_from_url( 'http://somedomain.com/nonexistent-image.png' );
+		$expected_error_message = 'Error getting remote image http://cldup.com/nonexistent-image.png. Error: Not Found';
+		$result                 = wc_rest_upload_image_from_url( 'http://cldup.com/nonexistent-image.png' );
 
 		$this->assertIsWPError( $result );
 		$this->assertEquals( $expected_error_message, $result->get_error_message() );
@@ -81,14 +52,14 @@ class WC_Tests_API_Functions extends WC_Unit_Test_Case {
 	public function test_wc_rest_upload_image_from_url_should_return_error_when_invalid_image_is_passed() {
 		// empty file.
 		$expected_error_message = 'Invalid image: File is empty. Please upload something more substantial. This error could also be caused by uploads being disabled in your php.ini or by post_max_size being defined as smaller than upload_max_filesize in php.ini.';
-		$result                 = wc_rest_upload_image_from_url( 'http://somedomain.com/invalid-image-1.png' );
+		$result                 = wc_rest_upload_image_from_url( 'https://cldup.com/raDaVDNJBy.txt' );
 
 		$this->assertIsWPError( $result );
 		$this->assertEquals( $expected_error_message, $result->get_error_message() );
 
 		// unsupported mime type.
 		$expected_error_message = 'Invalid image: Sorry, this file type is not permitted for security reasons.';
-		$result                 = wc_rest_upload_image_from_url( 'http://somedomain.com/invalid-image-2.png' );
+		$result                 = wc_rest_upload_image_from_url( 'https://cldup.com/pHSxq-xBwH.txt' );
 
 		$this->assertIsWPError( $result );
 		$this->assertEquals( $expected_error_message, $result->get_error_message() );
@@ -99,12 +70,14 @@ class WC_Tests_API_Functions extends WC_Unit_Test_Case {
 	 * information about it.
 	 */
 	public function test_wc_rest_upload_image_from_url_should_download_image_and_return_array() {
+		$upload_dir_info       = wp_upload_dir();
+
 		$expected_result = array(
-			'file' => $this->upload_dir_path . '/' . $this->file_name,
-			'url'  => $this->upload_dir_url . '/' . $this->file_name,
+			'file' => $upload_dir_info['path'] . '/Dr1Bczxq4q.png',
+			'url'  => $upload_dir_info['url'] . '/Dr1Bczxq4q.png',
 			'type' => 'image/png',
 		);
-		$result          = wc_rest_upload_image_from_url( 'http://somedomain.com/' . $this->file_name );
+		$result          = wc_rest_upload_image_from_url( 'http://cldup.com/Dr1Bczxq4q.png' );
 
 		$this->assertEquals( $expected_result, $result );
 	}
@@ -196,52 +169,5 @@ class WC_Tests_API_Functions extends WC_Unit_Test_Case {
 	 */
 	public function test_wc_rest_check_manager_permissions() {
 		$this->assertFalse( wc_rest_check_manager_permissions( 'reports' ) );
-	}
-
-	/**
-	 * Helper method to define mocked HTTP responses using WP_HTTP_TestCase.
-	 * Thanks to WP_HTTP_TestCase, it is not necessary to perform a regular request
-	 * to an external server which would significantly slow down the tests.
-	 *
-	 * This function is called by WP_HTTP_TestCase::http_request_listner().
-	 *
-	 * @param array  $request Request arguments.
-	 * @param string $url URL of the request.
-	 *
-	 * @return array|false mocked response or false to let WP perform a regular request.
-	 */
-	protected function mock_http_responses( $request, $url ) {
-		$mocked_response = false;
-
-		if ( 'http://somedomain.com/nonexistent-image.png' === $url ) {
-			$mocked_response = array(
-				'response' => array(
-					'code'    => 404,
-					'message' => 'Not found.',
-				),
-			);
-		} elseif ( 'http://somedomain.com/invalid-image-1.png' === $url ) {
-			// empty image.
-			$mocked_response = array(
-				'response' => array( 'code' => 200 ),
-			);
-		} elseif ( 'http://somedomain.com/invalid-image-2.png' === $url ) {
-			// image with an unsupported mime type.
-			// we need to manually copy the file as we are mocking the request. without this an empty file is created.
-			copy( WC_Unit_Tests_Bootstrap::instance()->tests_dir . '/data/file.txt', $request['filename'] );
-
-			$mocked_response = array(
-				'response' => array( 'code' => 200 ),
-			);
-		} elseif ( 'http://somedomain.com/' . $this->file_name === $url ) {
-			// we need to manually copy the file as we are mocking the request. without this an empty file is created.
-			copy( WC_Unit_Tests_Bootstrap::instance()->tests_dir . '/data/Dr1Bczxq4q.png', $request['filename'] );
-
-			$mocked_response = array(
-				'response' => array( 'code' => 200 ),
-			);
-		}
-
-		return $mocked_response;
 	}
 }
